14fd48a7cce16c7bd6f9a326062a5cd6589ced52
Ebi27 <kpemiogokimiebi@gmail.com>
2023-05-10 12:58:32 +0200

    Create playlist challenge to add, remove and check playlist length

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,2 +1,35 @@
+
+/*
+Challenge
+Build Playlist.java with the best possible playlist of music using a Java ArrayList
+
+This is to practice array and arrayLists as well as the syntax.
+ */
+import java.util.ArrayList;
 public class Playlist {
+    public static void main(String[] args){
+        ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+        //add songs to your playlist
+        desertIslandPlaylist.add("Doin' Time");
+        desertIslandPlaylist.add("West Coast");
+        desertIslandPlaylist.add("Cinnamon Girl");
+        desertIslandPlaylist.add("Money Power Glory");
+        desertIslandPlaylist.add("Aint No Sunshine");
+        desertIslandPlaylist.add("D.S");
+        desertIslandPlaylist.add("Thique");
+        //check how many songs you currently have in your playlist
+        System.out.println(desertIslandPlaylist);
+        System.out.println(desertIslandPlaylist.size());
+
+        //remove 2 songs from your playlist
+        desertIslandPlaylist.remove(3);
+        desertIslandPlaylist.remove(4);
+
+        //check how many songs you currently have in your playlist
+        System.out.println(desertIslandPlaylist);
+        System.out.println(desertIslandPlaylist.size());
+
+        //Swap the position of one music from the playlist with another
+
+    }
 }
